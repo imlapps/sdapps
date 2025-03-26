c87658dd2a18a84856c5281f5d2f18f3e82fe638
Minor Gordon <minorg@server.fake>
2025-03-26 11:23:09 -0400

    fix towndex model set tests

--- packages/models/__tests__/behavesLikeTowndexModelSet.ts
+++ packages/models/__tests__/behavesLikeTowndexModelSet.ts
@@ -7,33 +7,38 @@ export function behavesLikeTowndexModelSet(modelSet: ModelSet): void {
     const events = (await modelSet.models<Event>("Event")).unsafeCoerce();
     expect(events).toHaveLength(113);
     expect(
-      events.filter((event) => event.startDate.isJust() && event.name.isJust()),
-    ).toHaveLength(26);
+      events.filter(
+        (event) =>
+          event.startDate.isJust() &&
+          event.name.isJust() &&
+          event.superEvent.isNothing(),
+      ),
+    ).toHaveLength(17);
   });
 
   it("event count", async ({ expect }) => {
-    expect((await modelSet.modelCount("Event")).unsafeCoerce()).toEqual(0);
+    expect((await modelSet.modelCount("Event")).unsafeCoerce()).toEqual(113);
   });
 
   it("organization stubs", async ({ expect }) => {
     expect(
       (await modelSet.models("OrganizationStub")).unsafeCoerce(),
-    ).toHaveLength(0);
+    ).toHaveLength(3);
   });
 
   it("organization count", async ({ expect }) => {
     expect((await modelSet.modelCount("Organization")).unsafeCoerce()).toEqual(
-      0,
+      3,
     );
   });
 
   it("person stubs", async ({ expect }) => {
     expect((await modelSet.models("PersonStub")).unsafeCoerce()).toHaveLength(
-      58,
+      55,
     );
   });
 
   it("person count", async ({ expect }) => {
-    expect((await modelSet.modelCount("Person")).unsafeCoerce()).toEqual(58);
+    expect((await modelSet.modelCount("Person")).unsafeCoerce()).toEqual(55);
   });
 }
