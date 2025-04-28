f2439d96877a47f13bbeb74727f9f77d368123c0
diahelicoprion00 <reedishigamiwayne00@gmail.com>
2025-04-28 17:03:21 -0400

    feat: added collate_rdf

---
+++ apps/towndex_etl/towndex_etl/collate_rdf.py
@@ -0,0 +1,80 @@
+import json
+import pprint
+from pathlib import Path
+import pyoxigraph as ox
+from rdflib import Graph, Dataset
+from rdflib import Literal, Namespace, URIRef
+from rdflib.graph import ConjunctiveGraph, Graph
+from rdflib.plugins.stores.memory import Memory
+
+from rdflib import Graph, Literal, RDF, URIRef
+
+# rdflib knows about quite a few popular namespaces, like W3C ontologies, schema.org etc.
+from rdflib.namespace import FOAF, XSD
+
+from models.schema_class_relationships import SCHEMA_CLASS_RELATIONSHIPS
+
+
+def collate_rdf(meeting_minutes_directory_path: Path) -> None:
+    """Read in RDF graphs and collate them in an RDF dataset."""
+    
+    store = Memory()
+    towndex_dataset = Dataset(store=store)
+
+    TOB = Namespace("https://townofbrunswick.org/files/")
+
+    towndex_dataset.bind("tob", TOB)
+    towndex_dataset.bind("tdx", Namespace("http://purl.org/towndex/instance/us/ny/brunswick/"))
+    towndex_dataset.bind("rdfs", Namespace("http://www.w3.org/2000/01/rdf-schema#"))
+    towndex_dataset.bind("sdo", Namespace("http://schema.org/"))
+
+    for schema_type in SCHEMA_CLASS_RELATIONSHIPS.keys():
+        towndex_dataset.add((
+            URIRef("http://schema.org/" + schema_type),
+            URIRef("http://www.w3.org/2000/01/rdf-schema#subClassOf"),
+            URIRef("http://schema.org/" + SCHEMA_CLASS_RELATIONSHIPS[schema_type]),
+        ))
+
+    for meeting_file_path in list(
+        Path(__file__)
+        .parent.absolute()
+        .glob( meeting_minutes_directory_path/ Path("*.jsonld"))
+    ):
+        
+        g = towndex_dataset.graph(URIRef("https://townofbrunswick.org/files/" + meeting_file_path.stem))
+        meeting_minutes_graph = Graph()
+        meeting_minutes_graph.parse(meeting_file_path)
+        for triples in meeting_minutes_graph:
+            pprint.pprint(triples)
+            g.add(triples)
+
+        
+        
+        minutes_file = json.load(meeting_file_path.open(encoding="utf-8"))
+        document_graph = [
+            (
+                URIRef(minutes_file[0]["url"].replace(" ","%20")),
+                RDF.type,
+                URIRef("http://schema.org/TextObject"),
+            ),
+            (
+            URIRef(minutes_file[0]["url"].replace(" ","%20")),
+                URIRef("http://schema.org/url"),
+                URIRef(minutes_file[0]["url"].replace(" ","%20")),
+            ),
+            (
+                URIRef(minutes_file[0]["url"].replace(" ","%20")),
+                URIRef("http://schema.org/about"),
+                URIRef(minutes_file[1]["@id"]),
+            ),   (
+                URIRef(minutes_file[0]["url"].replace(" ","%20")),
+            URIRef("http://schema.org/name"),
+                Literal(minutes_file[0]["url"][len(TOB):][:-4]),
+            )
+        ]
+        for triples in document_graph:
+            g.add(triples)
+
+
+    towndex_dataset.serialize(format="trig", destination= Path("./us/ny/brunswick/minutes.trig"))
+
--- apps/towndex_etl/towndex_etl/main.py
+++ apps/towndex_etl/towndex_etl/main.py
@@ -1,65 +1,11 @@
-from pathlib import Path
-
 from models.settings import Settings
-from read_meeting_minutes import read_meeting_minutes
-from towndex_kg_builder import TowndexKgBuilder
-from validate_towndex_kg import validate_towndex_kg
 
-
-def main(  # noqa: PLR0913
-    meeting_minutes_directory_path: Path,
-    meeting_minutes_files_start_index: int,
-    openai_model_prompt_file_path: Path,
-    shacl_shapes_graph_file_path: Path,
-    shacl_validator_cache_directory_path: Path,
-    towndex_kg_builder_output_directory_path: Path,
-) -> None:
-    
-    minutes_files = list(
-        read_meeting_minutes(
-            meeting_minutes_directory_path=meeting_minutes_directory_path
-        )
-    )
-
-    if minutes_files:
-        towndex_kg_builder = TowndexKgBuilder(
-            towndex_kg_builder_output_directory_path=towndex_kg_builder_output_directory_path,
-            openai_model_prompt_file_path=openai_model_prompt_file_path,
-        )
-
-        minutes_files_index = meeting_minutes_files_start_index
-        while True:
-            if minutes_files_index == len(minutes_files):
-                break
-            
-            # TowndexKgPipeline
-            validate_towndex_kg(
-                data_graph_path=towndex_kg_builder.build_graph(
-                    meeting_minutes=minutes_files[minutes_files_index]
-                ),
-                shapes_graph_path=shacl_shapes_graph_file_path,
-                shacl_validator_cache_directory_path=shacl_validator_cache_directory_path,
-            )
-
-            while True:
-                user_response = int(
-                    input(
-                        f"Completed extraction for meeting minutes with document ID: {minutes_files[minutes_files_index].id}.\n\
-                         Evaluate SHACL validation report and enter a number to continue:\n\n[1] Extract next meeting minutes\n[2] Re-run pipeline\n\nAnswer:"
-                    )
-                )
-                match user_response:
-                    case 1:
-                        minutes_files_index = minutes_files_index + 1
-                        break
-                    case 2:
-                        break
-                    case _:
-                        pass
+from collate_rdf import collate_rdf
+from parse_meeting_minutes import parse_meeting_minutes
 
 
 settings = Settings()
-main(
+parse_meeting_minutes(
     meeting_minutes_directory_path=settings.meeting_minutes_directory_path,
     meeting_minutes_files_start_index=settings.meeting_minutes_files_start_index,
     openai_model_prompt_file_path=settings.openai_model_prompt_file_path,
@@ -67,3 +13,6 @@ main(
     shacl_validator_cache_directory_path=settings.shacl_validator_cache_directory_path,
     towndex_kg_builder_output_directory_path=settings.towndex_kg_builder_output_directory_path,
 )
+collate_rdf(meeting_minutes_directory_path=settings.towndex_kg_builder_output_directory_path)
+
+
