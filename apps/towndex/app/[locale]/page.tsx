c8616fd6a2f08acc017e41a67e058a1670c58b94
Minor Gordon <minorg@server.fake>
2025-05-31 12:22:24 -0400

    redirect to events page

--- apps/towndex/app/[locale]/page.tsx
+++ apps/towndex/app/[locale]/page.tsx
@@ -1,14 +1,10 @@
 import { PageMetadata } from "@/lib/PageMetadata";
-import { AppShell } from "@/lib/components/AppShell";
-import { ClientProvidersServer } from "@/lib/components/ClientProvidersServer";
-import { dataset } from "@/lib/dataset";
 import { getHrefs } from "@/lib/getHrefs";
-import { modelSet } from "@/lib/modelSet";
 import { Locale } from "@/lib/models/Locale";
 import { serverConfiguration } from "@/lib/serverConfiguration";
-import { Anchor, Table, TableTbody, TableTd, TableTr } from "@mantine/core";
 import { Metadata } from "next";
-import { getTranslations, setRequestLocale } from "next-intl/server";
+import { setRequestLocale } from "next-intl/server";
+import { redirect } from "next/navigation";
 
 interface LocalePageParams {
   locale: Locale;
@@ -21,50 +17,8 @@ export default async function LocalePage({
   setRequestLocale(locale);
 
   const hrefs = await getHrefs();
-  const translations = await getTranslations("LocalePage");
 
-  return (
-    <ClientProvidersServer>
-      <AppShell title={`Towndex: ${serverConfiguration.siteTitle}`}>
-        <Table striped withColumnBorders>
-          <TableTbody>
-            <TableTr>
-              <TableTd>
-                <Anchor href={hrefs.events}>{translations("Events")}</Anchor>
-              </TableTd>
-              <TableTd>
-                {(await modelSet.modelCount("EventStub")).unsafeCoerce()}
-              </TableTd>
-            </TableTr>
-            <TableTr>
-              <TableTd>
-                <Anchor href={hrefs.organizations}>
-                  {translations("Organizations")}
-                </Anchor>
-              </TableTd>
-              <TableTd>
-                {(await modelSet.modelCount("OrganizationStub")).unsafeCoerce()}
-              </TableTd>
-            </TableTr>
-            <TableTr>
-              <TableTd>
-                <Anchor href={hrefs.people}>{translations("People")}</Anchor>
-              </TableTd>
-              <TableTd>
-                {(await modelSet.modelCount("PersonStub")).unsafeCoerce()}
-              </TableTd>
-            </TableTr>
-            <TableTr>
-              <TableTd>{translations("Dataset")}</TableTd>
-              <TableTd>
-                {dataset.size} {translations("quads")}
-              </TableTd>
-            </TableTr>
-          </TableTbody>
-        </Table>
-      </AppShell>
-    </ClientProvidersServer>
-  );
+  redirect(hrefs.events);
 }
 
 export async function generateMetadata({
