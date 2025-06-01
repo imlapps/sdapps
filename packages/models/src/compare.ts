1f4bfef733bbebc45cd774735cd01332e0c9000d
Minor Gordon <minorg@server.fake>
2025-06-01 14:01:58 -0400

    formatting

--- packages/models/src/compare.ts
+++ packages/models/src/compare.ts
@@ -3,26 +3,26 @@ import type { Identifier } from "./Identifier.js";
 import { displayLabel } from "./displayLabel.js";
 
 export function compare<
-	ModelT extends {
-		identifier: Identifier;
-		name: Maybe<string>;
-		order: Maybe<number>;
-		startDate?: Maybe<Date>;
-	},
+  ModelT extends {
+    identifier: Identifier;
+    name: Maybe<string>;
+    order: Maybe<number>;
+    startDate?: Maybe<Date>;
+  },
 >(left: ModelT, right: ModelT): number {
-	if (left.startDate && right.startDate) {
-		const startTimeDiff =
-			left.startDate.map((date) => date.getTime()).orDefault(0) -
-			right.startDate.map((date) => date.getTime()).orDefault(0);
-		if (startTimeDiff !== 0) {
-			return startTimeDiff;
-		}
-	}
+  if (left.startDate && right.startDate) {
+    const startTimeDiff =
+      left.startDate.map((date) => date.getTime()).orDefault(0) -
+      right.startDate.map((date) => date.getTime()).orDefault(0);
+    if (startTimeDiff !== 0) {
+      return startTimeDiff;
+    }
+  }
 
-	const orderDiff = left.order.orDefault(0) - right.order.orDefault(0);
-	if (orderDiff !== 0) {
-		return orderDiff;
-	}
+  const orderDiff = left.order.orDefault(0) - right.order.orDefault(0);
+  if (orderDiff !== 0) {
+    return orderDiff;
+  }
 
-	return displayLabel(left).localeCompare(displayLabel(right));
+  return displayLabel(left).localeCompare(displayLabel(right));
 }
