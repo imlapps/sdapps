fec410c59ab25b0b999a045ef3ce1a3c4452e1d9
Minor Gordon <minorg@server.fake>
2025-08-01 14:37:15 -0400

    factor out fixture

--- packages/etl/__tests__/WikipediaEntityResolver.test.ts
+++ packages/etl/__tests__/WikipediaEntityResolver.test.ts
@@ -2,80 +2,27 @@ import * as dotenv from "dotenv";
 import { describe, it } from "vitest";
 import { WikipediaEntityResolver } from "../src/WikipediaEntityResolver.js";
 import { cachesDirectoryPath } from "./paths.js";
+import { wikipediaEntityResolverTestData } from "./wikipediaEntityResolverTestData.js";
 
 describe("WikipediaEntityResolver", () => {
   const sut = new WikipediaEntityResolver({ cachesDirectoryPath });
 
   dotenv.config();
 
-  it("single entity in few-shot examples", async ({ expect }) => {
-    const entities = (
-      await sut.resolve({ name: "Jean Philippe Rameau", role: "composer" })
-    ).unsafeCoerce();
-    expect(entities).toHaveLength(1);
-    expect(entities[0].urlTitle).toStrictEqual("Jean-Philippe_Rameau");
-  });
-
-  it("single entity not in few-shot examples", async ({ expect }) => {
-    const entities = (
-      await sut.resolve({ name: "George Frideric Handel", role: "composer" })
-    ).unsafeCoerce();
-    expect(entities).toHaveLength(1);
-    expect(entities[0].urlTitle).toStrictEqual("George_Frideric_Handel");
-  });
-
-  it("single entity non-extant", async ({ expect }) => {
-    const entities = (
-      await sut.resolve({ name: "Phoenicia Hartsmuth", role: "composer" })
-    ).unsafeCoerce();
-    expect(entities).toHaveLength(0);
-  });
-
-  it("multiple entities in few-shot examples", async ({ expect }) => {
-    const entities = (
-      await sut.resolve({
-        name: "Vienna Phil Orch,Levine, James",
-        role: "artist",
-      })
-    ).unsafeCoerce();
-    expect(entities).toHaveLength(2);
-    expect(
-      entities.some((entity) => entity.urlTitle === "Vienna_Philharmonic"),
-    ).toStrictEqual(true);
-    expect(
-      entities.some((entity) => entity.urlTitle === "James_Levine"),
-    ).toStrictEqual(true);
-  });
-
-  it("multiple entities not in few-shot examples", async ({ expect }) => {
-    const entities = (
-      await sut.resolve({
-        name: "Bylsma, Anner,Lamon, Jean,Tafelmusik",
-        role: "artist",
-      })
-    ).unsafeCoerce();
-    expect(entities).toHaveLength(3);
-    expect(
-      entities.some((entity) => entity.urlTitle === "Anner_Bylsma"),
-    ).toStrictEqual(true);
-    expect(
-      entities.some((entity) => entity.urlTitle === "Jeanne_Lamon"),
-    ).toStrictEqual(true);
-    expect(
-      entities.some(
-        (entity) => entity.urlTitle === "Tafelmusik_Baroque_Orchestra",
-      ),
-    ).toStrictEqual(true);
-  });
-
-  it("multiple entities some non-existent", async ({ expect }) => {
-    const entities = (
-      await sut.resolve({
-        name: "Vienna Philharmonic,Phoenicia Hartsmuth",
-        role: "artist",
-      })
-    ).unsafeCoerce();
-    expect(entities).toHaveLength(1);
-    expect(entities[0].urlTitle).toStrictEqual("Vienna_Philharmonic");
-  });
+  for (const [id, { input, expectedOutput }] of Object.entries(
+    wikipediaEntityResolverTestData,
+  )) {
+    it(id, async ({ expect }) => {
+      const actualEntities = (await sut.resolve(input)).unsafeCoerce();
+      expect(actualEntities).toHaveLength(expectedOutput.length);
+      for (const expectedEntity of expectedOutput) {
+        expect(
+          actualEntities.some(
+            (actualEntity) =>
+              actualEntity.urlTitle === expectedEntity.wikipediaUrlTitle,
+          ),
+        ).toStrictEqual(true);
+      }
+    });
+  }
 });
---
+++ packages/etl/__tests__/wikipediaEntityResolverTestData.ts
@@ -0,0 +1,88 @@
+import { WikipediaEntityResolver } from "../src/WikipediaEntityResolver";
+
+export const wikipediaEntityResolverTestData: Record<
+  string,
+  {
+    input: Parameters<WikipediaEntityResolver["resolve"]>[0];
+    expectedOutput: readonly {
+      readonly wikidataId: string;
+      readonly wikipediaUrlTitle: string;
+    }[];
+  }
+> = {
+  singleEntityInFewShotExamples: {
+    input: { name: "Jean Philippe Rameau", role: "composer" },
+    expectedOutput: [
+      {
+        wikidataId: "Q1145",
+        wikipediaUrlTitle: "Jean-Philippe_Rameau",
+      },
+    ],
+  },
+
+  singleEntityNotInFewShotExamples: {
+    input: { name: "George Frideric Handel", role: "composer" },
+    expectedOutput: [
+      {
+        wikidataId: "Q7302",
+        wikipediaUrlTitle: "George_Frideric_Handel",
+      },
+    ],
+  },
+
+  singleEntityNonExtant: {
+    input: { name: "Phoenicia Hartsmuth", role: "composer" },
+    expectedOutput: [],
+  },
+
+  multipleEntitiesInFewShotExamples: {
+    input: {
+      name: "Vienna Phil Orch,Levine, James",
+      role: "artist",
+    },
+    expectedOutput: [
+      {
+        wikidataId: "Q336388",
+        wikipediaUrlTitle: "James_Levine",
+      },
+      {
+        wikidataId: "Q154685",
+        wikipediaUrlTitle: "Vienna_Philharmonic",
+      },
+    ],
+  },
+
+  multipleEntitiesNotInFewShotExamples: {
+    input: {
+      name: "Bylsma, Anner,Lamon, Jean,Tafelmusik",
+      role: "artist",
+    },
+    expectedOutput: [
+      {
+        wikidataId: "Q566461",
+        wikipediaUrlTitle: "Anner_Bylsma",
+      },
+      {
+        wikidataId: "Q539291",
+        wikipediaUrlTitle: "Jeanne_Lamon",
+      },
+      {
+        wikidataId: "Q218658",
+        wikipediaUrlTitle: "Tafelmusik_Baroque_Orchestra",
+      },
+    ],
+  },
+
+  multipleEntitiesSomeNonExistent: {
+    input: {
+      name: "Vienna Philharmonic,Phoenicia Hartsmuth",
+      role: "artist",
+    },
+    expectedOutput: [
+      {
+        wikidataId: "Q154685",
+        wikipediaUrlTitle: "Vienna_Philharmonic",
+      },
+    ],
+  },
+};
