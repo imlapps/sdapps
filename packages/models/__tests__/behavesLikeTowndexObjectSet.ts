efebe4f8fdf5e28e7c9dae092f164fdc697c91b7
Minor Gordon <minorg@server.fake>
2025-08-07 15:49:46 -0400

    adapt tests to new Towndex data

--- packages/models/__tests__/behavesLikeTowndexObjectSet.ts
+++ packages/models/__tests__/behavesLikeTowndexObjectSet.ts
@@ -5,7 +5,7 @@ import { $ObjectSet } from "../src/generated.js";
 export function behavesLikeTowndexObjectSet(objectSet: $ObjectSet): void {
   it("events", async ({ expect }) => {
     const events = Either.rights(await objectSet.events());
-    expect(events).toHaveLength(113);
+    expect(events).toHaveLength(34);
     expect(
       events.filter(
         (event) =>
@@ -13,26 +13,26 @@ export function behavesLikeTowndexObjectSet(objectSet: $ObjectSet): void {
           event.name.isJust() &&
           event.superEvent.isNothing(),
       ),
-    ).toHaveLength(17);
+    ).toHaveLength(4);
   });
 
   it("event count", async ({ expect }) => {
-    expect((await objectSet.eventsCount()).unsafeCoerce()).toEqual(113);
+    expect((await objectSet.eventsCount()).unsafeCoerce()).toEqual(34);
   });
 
   it("organization stubs", async ({ expect }) => {
-    expect(Either.rights(await objectSet.organizationStubs())).toHaveLength(3);
+    expect(Either.rights(await objectSet.organizationStubs())).toHaveLength(2);
   });
 
   it("organization count", async ({ expect }) => {
-    expect((await objectSet.organizationsCount()).unsafeCoerce()).toEqual(3);
+    expect((await objectSet.organizationsCount()).unsafeCoerce()).toEqual(2);
   });
 
   it("person stubs", async ({ expect }) => {
-    expect(Either.rights(await objectSet.personStubs())).toHaveLength(52);
+    expect(Either.rights(await objectSet.personStubs())).toHaveLength(37);
   });
 
   it("person count", async ({ expect }) => {
-    expect((await objectSet.peopleCount()).unsafeCoerce()).toEqual(52);
+    expect((await objectSet.peopleCount()).unsafeCoerce()).toEqual(37);
   });
 }
