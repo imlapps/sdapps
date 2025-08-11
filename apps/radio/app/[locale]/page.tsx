5d9d2df1fd331f35df80c4c5b7d9d8d129d4d704
Minor Gordon <minorg@server.fake>
2025-08-11 14:42:58 -0400

    stub out root page

---
+++ apps/radio/app/[locale]/page.tsx
@@ -0,0 +1,36 @@
+import { PageMetadata } from "@/lib/PageMetadata";
+import { Locale } from "@/lib/models/Locale";
+import { serverConfiguration } from "@/lib/serverConfiguration";
+import { Metadata } from "next";
+import { setRequestLocale } from "next-intl/server";
+
+interface LocalePageParams {
+  locale: Locale;
+}
+
+export default async function LocalePage({
+  params,
+}: { params: Promise<LocalePageParams> }) {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
+  throw new Error("locale page");
+  // redirect(new Hrefs({ basePath: "", locale }).events);
+}
+
+export async function generateMetadata({
+  params,
+}: { params: Promise<LocalePageParams> }): Promise<Metadata> {
+  const { locale } = await params;
+  return (await PageMetadata.get({ locale })).locale;
+}
+
+export function generateStaticParams(): LocalePageParams[] {
+  if (serverConfiguration.dynamic) {
+    return [];
+  }
+
+  return serverConfiguration.locales.map((locale) => ({
+    locale,
+  }));
+}
---
+++ apps/radio/app/layout.tsx
@@ -0,0 +1,18 @@
+import "@mantine/core/styles.layer.css";
+import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
