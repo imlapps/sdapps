e78a3f37bbc6a8f442a8df6332cd4ce007c0ea07
Minor Gordon <minorg@server.fake>
2025-08-11 17:29:40 -0400

    refactor to load directly into the Oxigraph store

--- apps/radio/lib/objectSet.ts
+++ apps/radio/lib/objectSet.ts
@@ -1,10 +1,76 @@
-import { dataset } from "@/lib/dataset";
-import { GlobalRef } from "@kos-kit/next-utils/server";
-import { $ObjectSet, $RdfjsDatasetObjectSet } from "@sdapps/models";
+import fs from "node:fs/promises";
+import { logger } from "@/lib/logger";
+import { serverConfiguration } from "@/lib/serverConfiguration";
+import {
+  GlobalRef,
+  RdfDirectory,
+  fsEither,
+  getRdfFileFormat,
+} from "@kos-kit/next-utils/server";
+import { OxigraphSparqlClient } from "@kos-kit/sparql-client";
+import { $SparqlObjectSet } from "@sdapps/models";
+import N3 from "n3";
+import * as oxigraph from "oxigraph";
 
-const objectSetGlobalRef = new GlobalRef<$ObjectSet>("objectSet");
+const objectSetGlobalRef = new GlobalRef<$SparqlObjectSet>("objectSet");
 
 if (!objectSetGlobalRef.value) {
-  objectSetGlobalRef.value = new $RdfjsDatasetObjectSet({ dataset });
+  const store = new oxigraph.Store();
+  if (serverConfiguration.dataPaths.length > 0) {
+    logger.info(
+      "loading dataset from data paths: %s",
+      serverConfiguration.dataPaths.join(":"),
+    );
+
+    for (const dataPath of serverConfiguration.dataPaths) {
+      const absoluteDataPath = await fs.realpath(dataPath);
+      await (await fsEither.stat(absoluteDataPath))
+        .mapLeft(async (error) => {
+          logger.warn("error stat'ing %s: %s", absoluteDataPath, error.message);
+        })
+        .map(async (stat) => {
+          if (stat.isDirectory()) {
+            for await (const file of new RdfDirectory({
+              logger,
+              path: absoluteDataPath,
+            }).files()) {
+              store.load(file.path, { format: file.format.rdfFormat });
+            }
+          } else if (stat.isFile()) {
+            await getRdfFileFormat(absoluteDataPath)
+              .mapLeft(async (error: any) => {
+                logger.warn(
+                  "%s is not an RDF file: %s",
+                  absoluteDataPath,
+                  error.message,
+                );
+              })
+              .map((rdfFileFormat) => {
+                store.load(absoluteDataPath, {
+                  format: rdfFileFormat.rdfFormat,
+                });
+              })
+              .extract();
+          } else {
+            logger.warn("%s is not a directory or a file", absoluteDataPath);
+          }
+        })
+        .extract();
+    }
+
+    if (store.size === 0) {
+      logger.warn("empty dataset after loading data paths");
+    }
+  } else {
+    logger.warn("no data paths specified");
+  }
+
+  objectSetGlobalRef.value = new $SparqlObjectSet({
+    sparqlClient: new OxigraphSparqlClient({
+      dataFactory: N3.DataFactory,
+      store,
+      useDefaultGraphAsUnion: true,
+    }),
+  });
 }
-export const objectSet: $ObjectSet = objectSetGlobalRef.value;
+export const objectSet: $SparqlObjectSet = objectSetGlobalRef.value;
