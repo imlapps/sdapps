d12196132be3fd025e2c19b4fb36a1e46de22067
Minor Gordon <minorg@server.fake>
2025-08-26 18:06:16 -0400

    sketching out playlist model

---
+++ apps/radio/lib/models/Playlist.ts
@@ -0,0 +1,41 @@
+import {} from "@sdapps/models";
+import { z } from "zod";
+
+const identifierSchema = z.string();
+
+const artistSchema = z.object({
+  identifier: identifierSchema,
+  name: z.string(),
+});
+
+const composerSchema = artistSchema;
+
+const compositionSchema = z.object({
+  composerIdentifier: identifierSchema,
+  identifier: identifierSchema,
+  name: z.string(),
+});
+
+const dateTimeSchema = z.string().datetime({ offset: true });
+
+const itemSchema = z.object({
+  artistIdentifiers: z.array(identifierSchema),
+  endDate: dateTimeSchema,
+  composition: compositionSchema.optional(),
+  startDate: dateTimeSchema,
+});
+
+const episodeSchema = z.object({
+  endDate: dateTimeSchema,
+  identifier: identifierSchema,
+  items: z.array(itemSchema),
+  startDate: dateTimeSchema,
+});
+
+const playlistSchema = z.object({
+  artistsByIdentifier: z.record(identifierSchema, artistSchema),
+  composersByIdentifier: z.record(identifierSchema, composerSchema),
+  episodes: z.array(episodeSchema),
+});
+
+export type Playlist = z.infer<typeof playlistSchema>;
